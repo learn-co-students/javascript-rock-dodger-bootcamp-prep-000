a2704ce927bf3ee8c790f896b67be8198f6fc5a8
David Lewitan <dovidlwtn@gmail.com>
2016-09-14 01:15:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/**
- * Don't change these constants!
- */
+
 const DODGER = document.getElementById('dodger')
 const GAME = document.getElementById('game')
 const GAME_HEIGHT = 400
@@ -12,17 +10,9 @@ const START = document.getElementById('start')
 
 var gameInterval = null
 
-/**
- * All of your work should happen below.
- */
-
-
 function checkCollision(rock) {
   const top = positionToInteger(rock.style.top)
 
-  // rocks are 20px high
-  // DODGER is 20px high
-  // GAME_HEIGHT - 20 - 20 = 360px;
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
     const dodgerRightEdge = dodgerLeftEdge + 40;
@@ -40,39 +30,33 @@ function checkCollision(rock) {
 
 function createRock(x) {
   const rock = document.createElement('div')
-  var top = 0;
 
-  rock.className = 'rock'
-  rock.style.left = `${x}px`
-  rock.style.top = `${top}px`;
+  rock.className = 'rock';
+  rock.style.left = `${x}px`;
+  rock.style.top = '0px';
   GAME.appendChild(rock);
 
+  var top = 0;
+
   function moveRock() {
-    top += 2;
-    rock.style.top = `${top}px`;
-
-    if (rock.parentNode && checkCollision(rock)) {
-      endGame();
-      top = 400;
-    }
-
-    if (top < 400) {
-      window.requestAnimationFrame(moveRock);
-    } else {
+    if (top >= 400) {
       rock.remove();
+    } else if (checkCollision(rock)) {
+      endGame();
+    } else {
+      rock.style.top = `${top += 2}px`;
+      window.requestAnimationFrame(moveRock);
     }
   }
 
-  // We should kick of the animation of the rock around here
   window.requestAnimationFrame(moveRock);
-  // Add the rock to ROCKS so that we can remove all rocks
-  // when there's a collision
+
   ROCKS.push(rock)
   return rock
 }
 
 function endGame() {
-  window.removeEventListener('keydown', moveDodger)
+  document.removeEventListener('keydown', moveDodger);
   window.clearInterval(gameInterval);
   ROCKS.forEach(function(rock){
     rock.remove();
@@ -81,18 +65,18 @@ function endGame() {
 }
 
 function moveDodger(e) {
-   e.preventDefault();
-   e.stopPropagation();
-   if (e.which == 37) {
-     moveDodgerLeft();
-   }
-   if (e.which == 39) {
-     moveDodgerRight()
+//  var act = {37: moveDodgerLeft, 38: moveDodgerRight};
+
+   if (e.which == 37 || e.which == 39) {
+      e.preventDefault();
+      e.stopPropagation();
+      e.which == 37 ? moveDodgerLeft() : false;
+      e.which == 39 ? moveDodgerRight() : false;
    }
 }
 
 function moveDodgerLeft() {
-  var move = 4;
+  var move = 5;
 
   function stepLeft() {
     if (positionToInteger(DODGER.style.left) > 0) {
@@ -101,15 +85,14 @@ function moveDodgerLeft() {
       return
     }
 
-    move--;
-    move > 0 ? window.requestAnimationFrame(stepLeft) : false;
+    move-- > 0 ? window.requestAnimationFrame(stepLeft) : false;
   }
 
   window.requestAnimationFrame(stepLeft);
 }
 
 function moveDodgerRight() {
-  var move = 4;
+  var move = 5;
 
   function stepRight() {
     if (positionToInteger(DODGER.style.left) < 360) {
@@ -118,17 +101,12 @@ function moveDodgerRight() {
       return
     }
 
-    move--;
-    move > 0 ? window.requestAnimationFrame(stepRight) : false;
+    move-- > 0 ? window.requestAnimationFrame(stepRight) : false;
   }
 
   window.requestAnimationFrame(stepRight);
 }
 
-/**
- * @param {string} p The position property
- * @returns {number} The position as an integer (without 'px')
- */
 function positionToInteger(p) {
   return parseInt(p.split('px')[0]) || 0
 }
