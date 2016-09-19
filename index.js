bcd015a790da4128390c7d8c632b668c068f367a
Alejandro Coca <amcocarojas@gmail.com>
2016-09-19 00:20:57 +0000

    Done.

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function checkCollision(rock) {
   // implement me!
   // use the comments below to guide you!
   const top = positionToInteger(rock.style.top)
+  //var top = positionToInteger(rock.style.top)
 
   // rocks are 20px high
   // DODGER is 20px high
@@ -28,12 +29,10 @@ function checkCollision(rock) {
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
     const dodgerRightEdge = dodgerLeftEdge + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
     const rockRightEdge = rockLeftEdge + 20;
 
     /**
@@ -64,7 +63,7 @@ function createRock(x) {
   // Hmmm, why would we have used `var` here?
 
 
-  var top = rock.style.top = 0;
+  top = rock.style.top = 0;
 
   /**
    * Now that we have a rock, we'll need to append
@@ -86,6 +85,7 @@ function createRock(x) {
      */
      if (checkCollision(rock)) {
        endGame();
+       break;
      }
 
     /**
