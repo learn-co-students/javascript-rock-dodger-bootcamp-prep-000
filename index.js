eca4b80313c664bc9e4979ed0b7e3c2874bacd23
Matthew Palumbo <mmpal78@yahoo.com>
2016-10-17 14:52:54 +0000

    Done.

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function createRock(x) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
-     
+
      if(parseInt(rock.style.top) > GAME_HEIGHT){
        GAME.removeChild(rock);
      }
