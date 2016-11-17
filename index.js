4dd309a5497a225e6fa6646d6cb5bb3da5db1953
John Shelby <johnshelby@yahoo.com>
2016-11-17 17:30:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*const DODGER = document.getElementById('dodger')
+const DODGER = document.getElementById('dodger')
 const GAME = document.getElementById('game')
 const GAME_HEIGHT = 400
 const GAME_WIDTH = 400
@@ -90,12 +90,16 @@ function moveDodgerRight() {
 }
 
 function endGame() {
-clearInterval(gameInterval)
-for(i=0,l=ROCKS.length;i<l;i++){
-    ROCKS[0].remove()
-}
-document.removeEventListener('keydown',moveDodger)
-alert('YOU LOSE')
+  clearInterval(gameInterval)
+
+  ROCKS.forEach(function(rock) { rock.remove() })
+
+  document.removeEventListener('keydown', moveDodger)
+
+  START.innerHTML = 'Play again?'
+  START.style.display = 'inline'
+
+  return alert('YOU LOSE!')
 }
 
 function positionToInteger(p) {
@@ -104,121 +108,6 @@ function positionToInteger(p) {
 
 function start() {
   document.addEventListener('keydown', moveDodger)
-  START.style.display = 'none'
-  gameInterval = setInterval(function() {
-    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  }, 1000)
-}
-*/
-
-const DODGER = document.getElementById('dodger')
-const GAME = document.getElementById('game')
-const GAME_HEIGHT = 400
-const GAME_WIDTH = 400
-const LEFT_ARROW = 37 // use e.which!
-const RIGHT_ARROW = 39 // use e.which!
-const ROCKS = []
-const START = document.getElementById('start')
-var gameInterval = null
-var done = false
-
-function checkCollision(rock) {
-  const top = positionToInteger(rock.style.top)
-  if (top > 360) {
-    const dodgerLeftEdge = positionToInteger(DODGER.style.left)
-    const dodgerRightEdge = dodgerLeftEdge + 40;
-    const rockLeftEdge = positionToInteger(rock.style.left)
-    const rockRightEdge = rockLeftEdge + 20;
-    if
-	(((rockRightEdge  >= dodgerLeftEdge) && (rockRightEdge <= dodgerRightEdge)) ||
-	((rockLeftEdge >= dodgerLeftEdge) && (rockLeftEdge <= dodgerRightEdge))) {
-      return true
-    }
-  }
-}
-
-function createRock(x) {
- 	const rock = document.createElement('div')
-  	rock.className = 'rock'
-  	rock.style.left = `${x}px`
-  	var top = 0
-  	rock.style.top = top
-	GAME.appendChild(rock)
-
-     function  moveRock(){
-     		function step(){
-     		if (done){
-     		return}
-     		rock.style.top = `${top+= 2}px`
-     		topNow=positionToInteger(rock.style.top)
-     		if(checkCollision(rock)){
-				endGame()
-			}
-   	  	 	else if (topNow <380){
-   	  	 	window.requestAnimationFrame(step)
-   	  	 	}
-   	  	 	else {
-   	  	 	rock.remove()
-   	  	 }
-   	  	}
-   	  	     window.requestAnimationFrame(step)
-	}
-	moveRock()
-	ROCKS.push(rock)
-	return rock
-}
-
-function moveDodger(e) {
-	if(e.which===LEFT_ARROW){
-		moveDodgerLeft()
-	}
-	if(e.which===RIGHT_ARROW){
-		moveDodgerRight()
-		}
-	}
-
-function moveDodgerLeft() {
-  function step(){
-  	var leftNumbers = dodger.style.left.replace('px', '')
-  	var left = parseInt(leftNumbers, 10)
-  		  if (left > 0){
-  			dodger.style.left = `${left - 4}px`;
-  			window.requestAnimationFrame(step)
-  		}
-	 }
-	 window.requestAnimationFrame(step)
-}
-
-function moveDodgerRight() {
-  function step(){
-  	var rightNumbers = dodger.style.left.replace('px', '')
-  	var right = parseInt(rightNumbers, 10)
-  		  if (right < 360){
-  			dodger.style.left = `${right + 4}px`;
-  			window.requestAnimationFrame(step)
-  		}
-	 }
-	 window.requestAnimationFrame(step)
-}
-
-function endGame() {
-clearInterval(gameInterval)
-var finalRocks=document.getElementsByClassName("rock")
-while(finalRocks.length > 0){
-	finalRocks[0].remove()
-	}
-window.removeEventListener('keydown',moveDodger)
-alert('YOU LOSE')
-done = true
-}
-
-function positionToInteger(p) {
-  return parseInt(p.split('px')[0]) || 0
-}
-
-function start() {
-debugger
-  window.addEventListener('keydown', moveDodger)
   START.style.display = 'none'
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
