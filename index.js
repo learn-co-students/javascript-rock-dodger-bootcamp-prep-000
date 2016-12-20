69049e62994e094077ae2ed0e3a5e7bd357b5da6
Raul Vasquez <rvasqz86@gmail.com>
2016-12-20 21:07:25 +0000

    Done.

--- index.js
+++ index.js
@@ -96,10 +96,12 @@ function moveDodger( e ) {
 	if ( e.which === LEFT_ARROW ) {
 		moveDodgerLeft()
 		e.stopPropagation()
+		e.preventDefault()
 	}
 	else if ( e.which === RIGHT_ARROW ) {
 		moveDodgerRight()
 		e.stopPropagation()
+		e.preventDefault()
 	}
 	else{
 		//e.preventDefault();
