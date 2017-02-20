305b1eec163561103aebd1274c1a8c08f23ec43e
Marina Lupin <lupinmarina@gmail.com>
2017-02-20 10:21:51 -0800

    Fix bug - rocks now fall instead of blinking on screen

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const LEFT_ARROW = 37; // use e.which!
 const RIGHT_ARROW = 39; // use e.which!
 const ROCKS = [];
 const START = document.getElementById('start');
-
+var gameStatus = false;
 var gameInterval = null;
 
 /**
@@ -23,19 +23,17 @@ function checkCollision(rock) {
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
-  if (top > 360) {
+  if (top > 360 && gameStatus) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left);
-
     const dodgerRightEdge = dodgerLeftEdge + 40;
-
+    
     const rockLeftEdge = positionToInteger(rock.style.left);
-
     const rockRightEdge = rockLeftEdge + 20;
 
     if (
-      (rockLeftEdge <= dodgerLeftEdge) && (rockRightEdge >= dodgerLeftEdge) ||
-      (rockLeftEdge <= dodgerRightEdge) && (rockRightEdge >= dodgerRightEdge) ||
-       (rockLeftEdge >= dodgerLeftEdge) && (rockRightEdge <= dodgerRightEdge)) {
+      (rockLeftEdge < dodgerLeftEdge) && (rockRightEdge > dodgerLeftEdge) ||
+      (rockLeftEdge < dodgerRightEdge) && (rockRightEdge > dodgerRightEdge) ||
+       (rockLeftEdge > dodgerLeftEdge) && (rockRightEdge < dodgerRightEdge)) {
        /**
                * Think about it -- what's happening here?
                * There's been a collision if one of three things is true:
@@ -55,8 +53,8 @@ function createRock(x) {
   const rock = document.createElement('div');
   rock.className = 'rock';
   rock.style.left = `${x}px`;
-  var top = 0;
-  top = rock.style.top;
+  var top = rock.style.top = 0;
+  
 
   /**
    * Now that we have a rock, we'll need to append
@@ -66,17 +64,17 @@ function createRock(x) {
    GAME.appendChild(rock);
 
   /**
-   * This function moves the rock. (2 pixels at a time
+   * This function moves the rock. (4 pixels at a time
    * seems like a good pace.)
    */
 function moveRock() {
-     rock.style.top = `${top += 2}px`;
+     rock.style.top = `${top += 4}px`;
      //if a rock collides with the dodger, call end game function.
      if (checkCollision(rock)===true) {
        return endGame();
      }
      //if the rock is not at the bottom, move the rock.
-     else if (top < GAME_HEIGHT) {
+     if (top < GAME_HEIGHT) {
        window.requestAnimationFrame(moveRock);
      }
      //if rock is at the bottom of the game, remove the rock.
@@ -171,5 +169,5 @@ function start() {
 
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  }, 2500)
+  }, 250)
 }
