50f2512efc34835d7c7a676ff0dfb458d4ac49c8
raj parsi <rajeshwar.parsi@gmail.com>
2017-03-28 22:33:15 +0000

    Done.

--- index.js
+++ index.js
@@ -29,25 +29,31 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge+40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const rockRightEdge = rockLeftEdge+20;
 
-    if (false /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
-               */) {
+    if(rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge  ||
+        rockLeftEdge >=dodgerLeftEdge && rockRightEdge <= dodgerLeftEdge  ||
+        rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge){
       return true
     }
+
+    // if (false /**
+    //            * Think about it -- what's happening here?
+    //            * There's been a collision if one of three things is true:
+    //            * 1. The rock's left edge is < the DODGER's left edge,
+    //            *    and the rock's right edge is > the DODGER's left edge;
+    //            * 2. The rock's left edge is > the DODGER's left edge,
+    //            *    and the rock's right edge is < the DODGER's right edge;
+    //            * 3. The rock's left edge is < the DODGER's right edge,
+    //            *    and the rock's right edge is > the DODGER's right edge
+    //            */) {
+    //   return true
+    // }
   }
 }
 
