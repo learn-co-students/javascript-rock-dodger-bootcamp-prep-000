56c78326fd9d0d2939d367fe31c893d3c4d123ea
David Schlauderaff <dschlaud@gmail.com>
2017-04-05 01:25:53 +0000

    Done.

--- index.js
+++ index.js
@@ -29,14 +29,17 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge + 40
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const rockRightEdge = rockLeftEdge + 20
 
-    if (false /**
+    if ( (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
+               (rockLeftEdge >= dodgerRightEdge && rockRightEdge <= dodgerRightEdge) ||
+               (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge))
+      /**
                * Think about it -- what's happening here?
                * There's been a collision if one of three things is true:
                * 1. The rock's left edge is < the DODGER's left edge,
@@ -45,7 +48,7 @@ function checkCollision(rock) {
                *    and the rock's right edge is < the DODGER's right edge;
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
-               */) {
+               */ {
       return true
     }
   }
@@ -66,15 +69,26 @@ function createRock(x) {
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-
+  GAME.appendChild(rock)
 
   /**
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
    */
   function moveRock() {
-    // implement me!
-    // (use the comments below to guide you!)
+
+      rock.style.top = `${top += 2}px`
+      if (checkCollision(rock)){
+        endGame()
+      } else if (top < GAME_HEIGHT) {
+        window.requestAnimationFrame(moveRock)
+      }
+      else if (top = GAME_HEIGHT) {
+        rock.remove()
+      }
+    }
+
+
     /**
      * If a rock collides with the DODGER,
      * we should call endGame()
@@ -89,10 +103,9 @@ function createRock(x) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
-  }
 
   // We should kick of the animation of the rock around here
-
+  window.requestAnimationFrame(moveRock)
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
@@ -108,6 +121,12 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
+  clearInterval(gameInterval)
+  ROCKS.forEach(function(rock) {
+    rock.remove()
+  })
+  document.removeEventListener('keydown', moveDodger)
+
 }
 
 function moveDodger(e) {
@@ -119,6 +138,18 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   const key = parseInt(e.detail||e.which)
+
+   if (key === LEFT_ARROW){
+     e.preventDefault()
+     e.stopPropagation()
+     moveDodgerLeft()
+   } else if (key === RIGHT_ARROW) {
+       e.preventDefault()
+    // e.stopPropagation()
+       moveDodgerRight()
+
+   }
 }
 
 function moveDodgerLeft() {
@@ -127,6 +158,13 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   window.requestAnimationFrame(function(){
+     const left = positionToInteger(DODGER.style.left)
+
+     if (left > 0){
+       DODGER.style.left = `${left - 4}px`
+     }
+   })
 }
 
 function moveDodgerRight() {
@@ -135,6 +173,13 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   window.requestAnimationFrame(function(){
+     const left = positionToInteger(DODGER.style.left)
+
+     if (left < 360){
+       DODGER.style.left = `${left + 4}px`
+     }
+   })
 }
 
 /**
