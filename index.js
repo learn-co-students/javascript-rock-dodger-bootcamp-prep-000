005b1a88ed274a75eed3de9522294ede54a33491
Nancy Chan <nancychannc@gmail.com>
2017-05-31 21:35:57 +0000

    Done.

--- index.js
+++ index.js
@@ -148,7 +148,12 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   if ([LEFT_ARROW, RIGHT_ARROW].indexOf(e.which) > -1) {
+
+  // if ([LEFT_ARROW, RIGHT_ARROW].indexOf(e.which) > -1) {
+  //   e.preventDefault()
+  //   e.stopPropagation()
+  // }
+  if (e.which === LEFT_ARROW || e.which === RIGHT_ARROW) {
     e.preventDefault()
     e.stopPropagation()
   }
