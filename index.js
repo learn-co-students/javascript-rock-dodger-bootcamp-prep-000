70fbe855dea9886b9d25765baea45a88b06bebf7
Justin Manalad <jstn.mnld@gmail.com>
2017-06-15 17:33:52 +0000

    Done.

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ function createRock(x) {
 
   // We should kick of the animation of the rock around here
 }
-  window.requestAnimationFrame(moveRock());
+  window.requestAnimationFrame();
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
