2de7fff9d2de5d0d3c8af20c093834219fd278db
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-22 13:37:45 +0000

    Done.

--- index.js
+++ index.js
@@ -29,25 +29,28 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const rockRightEdge = rockLeftEdge + 20;
 
-    if (false /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
-               */) {
-      return true
-    }
+    // if (false /**
+              // * Think about it -- what's happening here?
+              // * There's been a collision if one of three things is true:
+              //* 1. The rock's left edge is < the DODGER's left edge,
+              // *    and the rock's right edge is > the DODGER's left edge;
+              // * 2. The rock's left edge is > the DODGER's left edge,
+              // *    and the rock's right edge is < the DODGER's right edge;
+              // * 3. The rock's left edge is < the DODGER's right edge,
+              // *    and the rock's right edge is > the DODGER's right edge
+              // */) {
+      return (
+      (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
+      (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
+      (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
+  )
   }
 }
 
@@ -58,41 +61,40 @@ function createRock(x) {
   rock.style.left = `${x}px`
 
   // Hmmm, why would we have used `var` here?
-  var top = 0
-
-  rock.style.top = top
-
+  var top = rock.style.top = 0
+  GAME.appendChild(rock)
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-
-
   /**
    * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
+   * seems like a good pace.)*/
   function moveRock() {
+    rock.style.top = `${top +=2}px`;
+
+    if(checkCollision(rock)){
+      return endGame()
+    }
     // implement me!
     // (use the comments below to guide you!)
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame()
-     */
-
+    // * If a rock collides with the DODGER,
+    // * we should call endGame()
+    if (top<GAME_HEIGHT){
+      window.requestAnimationFrame(moveRock)
+    } else{
+      rock.remove()
+    }
     /**
      * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-
+     * the GAME, we want to move it again.*/
     /**
      * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
+     * we should remove the rock from the DOM */
   }
 
   // We should kick of the animation of the rock around here
-
+  window.requestAnimationFrame(moveRock)
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
@@ -108,6 +110,12 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
+  clearInterval(gameInterval)
+  ROCKS.forEach(function(rock){rock.remove()})
+  document.removeEventListener ('keydown', moveDodger)
+  START.innerHTML = 'Play again?'
+  START.style.display = 'inline'
+  return alert ('YOU LOSE!')
 }
 
 function moveDodger(e) {
