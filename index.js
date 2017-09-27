c45d50c361f4faa598141ee39e80f5fb33941e3e
Emma Konrad <emma.konrad92@gmail.com>
2017-09-27 16:28:03 +0000

    Done.

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function endGame() {
 
   ROCKS.forEach(function(rock) {rock.remove() })
 
-  document.removeEventListener('keydown', moveDodger);
+  window.removeEventListener('keydown', moveDodger);
 
   START.innerHTML = 'Play again?'
   START.style.display = 'inline'
