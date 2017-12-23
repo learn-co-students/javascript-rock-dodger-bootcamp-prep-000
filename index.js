005e60c6d3ec2386f17f5d2f8640cc5a280b6058
Helen Liutongco <liutongcoh@gmail.com>
2017-12-23 15:57:01 -0600

    Add existing file

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ var gameInterval = null
 function checkCollision(rock) {
   const top = positionToInteger(rock.style.top)
 
-  if (top > 360) {
+  if (top > 350) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
-    const dodgerRightEdge = dodgerLeftEdge+40;
+    const dodgerRightEdge = dodgerLeftEdge+35;
     const rockLeftEdge = positionToInteger(rock.style.left)
     const rockRightEdge = rockLeftEdge+20;
 
@@ -67,8 +67,7 @@ function endGame() {
 
   clearInterval(gameInterval);
   window.removeEventListener('keydown', moveDodger);
-  alert("YOU LOSE!");
-  START.innerHTML = 'Play again?'
+  START.innerHTML = 'You got caught! Play again?'
   START.style.display = 'inline'
 }
 
@@ -95,7 +94,7 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
   const left = positionToInteger(dodger.style.left);
-  if (left < GAME_WIDTH - 40){
+  if (left < GAME_WIDTH - 50){
     dodger.style.left = `${left + 2}px`;
     window.requestAnimationFrame(moveDodgerRight);
   }
@@ -114,6 +113,6 @@ function start() {
 
   START.style.display = 'none'
   gameInterval = setInterval(function() {
-      createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-    }, 1000)
+      createRock(Math.floor(Math.random() *  (GAME_WIDTH - 25)))
+    }, 250)
 }
