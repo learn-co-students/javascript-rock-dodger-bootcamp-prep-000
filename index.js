40b43666fcce877c1caef2c3562357c5f2d1e4a5
Betsy Sallee <ensallee@gmail.com>
2018-02-06 18:43:59 +0000

    Done.

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ function moveDodgerLeft(dodger) {
    */
    var leftNumbers=dodger.style.left.replace('px', '');
    var left=parseInt(leftNumbers, 10);
-   
+
    function step(){
      dodger.style.left=`${left-=4}px`;
       if (left<400){
@@ -152,11 +152,11 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-   var rightNumbers=dodger.style.right.replace('px', '');
+   var rightNumbers=DODGER.style.left.replace('px', '');
    var right=parseInt(rightNumbers, 10);
-   
+
    function step(){
-     dodger.style.right=`${right-=4}px`;
+     dodger.style.left=`${right+4}px`;
       if (right<400){
         window.requestAnimationFrame(step);
       }
