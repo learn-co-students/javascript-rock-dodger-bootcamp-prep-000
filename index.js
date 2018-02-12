e4783a515d9a83e74768888bfa31b8ed2d88aaaa
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-12 15:36:53 -0500

    small fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const GAME_HEIGHT = 400
 const GAME_WIDTH = 400
 const LEFT_ARROW = 37 // use e.which!
 const RIGHT_ARROW = 39 // use e.which!
-ROCKS = []
+const ROCKS = []
 const START = document.getElementById('start')
 
 var gameInterval = null
