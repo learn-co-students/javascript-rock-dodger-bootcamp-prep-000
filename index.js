82e2738d3f2183910b85e0c8fab06b923eb071f3
Susan <suzutoyuki@gmail.com>
2018-02-17 10:38:10 +0000

    Done.

--- index.js
+++ index.js
@@ -29,14 +29,14 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const rockRightEdge = rockLeftEdge + 20;
 
-    if (false /**
+     /**
                * Think about it -- what's happening here?
                * There's been a collision if one of three things is true:
                * 1. The rock's left edge is < the DODGER's left edge,
@@ -45,7 +45,12 @@ function checkCollision(rock) {
                *    and the rock's right edge is < the DODGER's right edge;
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
-               */) {
+               */
+
+  if ((rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
+(rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
+(rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)
+  ) {
       return true
     }
   }
@@ -66,7 +71,7 @@ function createRock(x) {
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-
+GAME.appendChild(rock)
 
   /**
    * This function moves the rock. (2 pixels at a time
@@ -79,20 +84,27 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
+     if (checkCollision(rock)){
+       return endGame();}
 
     /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
      */
-
+else if (top<400){
+  rock.style.top = `${top += 2}px`;
+  window.requestAnimationFrame(moveRock);
+}
     /**
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
-  }
+else if(top >=400) {
+  GAME.removeChild(rock);
+}}
 
   // We should kick of the animation of the rock around here
-
+window.requestAnimationFrame(moveRock);
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
@@ -108,6 +120,13 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
+
+  clearInterval(gameInterval);
+   ROCKS.forEach(function(rock)
+      {rock.remove()
+      });
+   window.removeEventListener('keydown', moveDodger);
+  return alert('YOU LOSE!');
 }
 
 function moveDodger(e) {
@@ -119,6 +138,17 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+
+   if([LEFT_ARROW, RIGHT_ARROW].indexOf(e.which) > -1){
+        e.preventDefault();
+        e.stopPropagation();
+}
+   if (e.which === LEFT_ARROW){
+       moveDodgerLeft()
+      }
+      if (e.which === RIGHT_ARROW){
+       moveDodgerRight()
+     }
 }
 
 function moveDodgerLeft() {
@@ -127,16 +157,29 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-}
+   window.requestAnimationFrame(function() {
+const left = positionToInteger(DODGER.style.left)
+  if (left > 0) {
+  DODGER.style.left = `${left - 4}px`;
+    }
+   })
+  }
 
-function moveDodgerRight() {
+  function moveDodgerRight(){
   // implement me!
   /**
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-}
 
+   window.requestAnimationFrame(function() {
+
+     const left = positionToInteger(DODGER.style.left)
+if (left < 360) {
+DODGER.style.left = `${left + 4}px`;
+    }
+   })
+}
 /**
  * @param {string} p The position property
  * @returns {number} The position as an integer (without 'px')
