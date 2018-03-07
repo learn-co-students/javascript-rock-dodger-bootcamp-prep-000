5d48f96e0eaa532c91deb7ff6110c39d7384f4ef
Mike McLean <mcleanmikejames@gmail.com>
2018-03-07 20:50:03 +0000

    Done.

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const DODGER = document.getElementById('dodger')
 const GAME = document.getElementById('game')
 const GAME_HEIGHT = 400
 const GAME_WIDTH = 400
-const LEFT_ARROW = 37 // use e.which!
-const RIGHT_ARROW = 39 // use e.which!
+const LEFT_ARROW = 37 // use e.which == 37 to id it
+const RIGHT_ARROW = 39 // use e.which == 39 to id it
 const ROCKS = []
 const START = document.getElementById('start')
 
@@ -20,39 +20,42 @@ var gameInterval = null
 function checkCollision(rock) {
   // implement me!
   // use the comments below to guide you!
-  const top = positionToInteger(rock.style.top)
-
+  const top = positionToInteger(rock.style.top);
+  
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
   if (top > 360) {
-    const dodgerLeftEdge = positionToInteger(DODGER.style.left)
+    const dodgerLeftEdge = positionToInteger(DODGER.style.left);
+    const dodgerRightEdge = dodgerLeftEdge + 40;
 
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const rockLeftEdge = positionToInteger(rock.style.left);
+    const rockRightEdge = rockLeftEdge + 20;
 
-    const rockLeftEdge = positionToInteger(rock.style.left)
 
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
-
-    if (false /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
-               */) {
-      return true
-    }
-  }
+        if ((rockLeftEdge < dodgerLeftEdge) && (rockRightEdge > dodgerLeftEdge)) 
+          {
+            return true;
+          }
+          
+        else if ((rockLeftEdge > dodgerLeftEdge) && (rockRightEdge < dodgerLeftEdge))
+          {
+            return true;
+          }
+          
+        else if ((rockLeftEdge < dodgerRightEdge) && (rockRightEdge > dodgerLeftEdge))
+          {
+            return true;
+          }
+        else 
+          {
+            return false;
+          }
+      }
 }
 
 function createRock(x) {
-  const rock = document.createElement('div')
+  const rock = document.createElement('div');
 
   rock.className = 'rock'
   rock.style.left = `${x}px`
@@ -62,37 +65,57 @@ function createRock(x) {
 
   rock.style.top = top
 
+  
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-
+  $('#game').append(rock);
 
   /**
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
    */
   function moveRock() {
-    // implement me!
+
+    
     // (use the comments below to guide you!)
+    
     /**
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-
+    if (checkCollision(rock) === TRUE )
+      {
+        endGame();
+      }
+    
     /**
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
+    else if (positionToInteger(rock.style.top) === 400)
+      {
+        $('.rock').remove();
+      }
+
+     /**
+     * Otherwise, if the rock hasn't reached the bottom of
+     * the GAME, we want to move it again.
+     */
+     else
+     {
+       rock.style.top = `${top +=2}px`
+     }
   }
 
-  // We should kick of the animation of the rock around here
+  // We should kick off the animation of the rock around here
 
+  if (top < 400) {
+    window.requestAnimationFrame(moveRock);
+  }
+
+window.requestAnimationFrame(moveRock);
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
