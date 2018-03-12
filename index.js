e39cf0667afc5444a6488e757f9b0d6423892cad
maxsmouha@gmail.com <maxsmouha@gmail.com>
2018-03-12 01:01:10 +0000

    Done.

--- index.js
+++ index.js
@@ -45,7 +45,11 @@ function checkCollision(rock) {
                *    and the rock's right edge is < the DODGER's right edge;
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
-               */) {
+               */
+              rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge ||
+              rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge ||
+              rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge
+             ) {
       return true
     }
   }
