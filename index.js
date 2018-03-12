76deef1782c31f2a45efc83a478c5e45ab93cc3f
maxsmouha@gmail.com <maxsmouha@gmail.com>
2018-03-12 13:31:44 +0000

    Done.

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = pDODGER.style.left + 40;
+    const dodgerRightEdge = (DODGER.style.left + 40);
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = rockLeftEdge 20;
+    const rockRightEdge = (rockLeftEdge + 20);
 
     if (false /**
                * Think about it -- what's happening here?
@@ -62,7 +62,7 @@ function createRock(x) {
   rock.style.left = `${x}px`
 
   // Hmmm, why would we have used `var` here?
-  var top = 0
+  var top = 0  //so that we can update the position of the rock -M
 
   rock.style.top = top
 
@@ -70,7 +70,7 @@ function createRock(x) {
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-
+   document.querySelector('game').appendChild(rock);
 
   /**
    * This function moves the rock. (2 pixels at a time
@@ -83,7 +83,9 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-
+      if () {
+        
+      }
     /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
