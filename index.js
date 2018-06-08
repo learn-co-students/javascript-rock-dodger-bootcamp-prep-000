169111c85f5e25f7d0671d75712eb320b5da21c1
Aiden Loyd <aidensaloyd@gmail.com>
2018-06-08 14:34:32 -0500

    Removed leftover comments.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const DODGER = document.getElementById('dodger');
 const GAME = document.getElementById('game');
 const GAME_HEIGHT = 400;
 const GAME_WIDTH = 400;
-const LEFT_ARROW = 37; // use e.which!
-const RIGHT_ARROW = 39; // use e.which!
+const LEFT_ARROW = 37;
+const RIGHT_ARROW = 39;
 const ROCKS = [];
 const START = document.getElementById('start');
 
