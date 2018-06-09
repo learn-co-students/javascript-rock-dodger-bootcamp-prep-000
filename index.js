c042b1ed32213d8e27b48568b73c1f1ba6c3d738
Trevor Jameson <tjameson15@gmail.com>
2018-06-09 15:03:45 +0000

    Done.

--- index.js
+++ index.js
@@ -123,8 +123,10 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  gameInterval = 0;
-  ROCKS = [];
+  clearInterval(gameInterval);
+  for (let i = 0; i < ROCKS.length; i++) {
+    ROCKS[i].remove();
+  }
   window.removeEventListener('keydown', moveDodger);
   alert("YOU LOSE!");
 }
@@ -142,9 +144,13 @@ function moveDodger(e) {
 
    if (e.which == LEFT_ARROW) {
     moveDodgerLeft();
+    e.preventDefault();
+    e.stopPropagation();
    }
    else if (e.which == RIGHT_ARROW) {
     moveDodgerRight();
+    e.preventDefault();
+    e.stopPropagation();
    }
 }
 
