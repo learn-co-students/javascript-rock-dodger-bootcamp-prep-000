9f0c438907a4b62a48f109b8f824662d604ff228
heii70 <williamgao70@yahoo.com>
2018-06-11 04:39:42 +0000

    Done.

--- index.js
+++ index.js
@@ -134,7 +134,10 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    
-   if(e.which)
+  if(e.which === LEFT_ARROW)
+    moveDodgerLeft();
+  else if(e.which === RIGHT_ARROW)
+    moveDodgerRight();
 }
 
 function moveDodgerLeft() {
