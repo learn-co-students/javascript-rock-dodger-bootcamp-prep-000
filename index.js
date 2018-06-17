7391d24de292a0c87cc9ac57d5b0867db68b6757
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-17 19:06:51 +0000

    Done.

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function checkCollision(rock) {
     const dodgerRightEdge = dodgerLeftEdge + 40;
     const rockLeftEdge = positionToInteger(rock.style.left)
     const rockRightEdge = rockLeftEdge + 20;
-    if ((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) or
-    (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) or
+    if ((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
+    (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
     (rockLeftEdge <= dodgerRighttEdge && rockRightEdge >= dodgerRightEdge)) {
       return true
     }
