71c5a998fbc1937ff84ab3341e09ade01de97ea0
Jae Yong Kim <kim900180@gmail.com>
2018-07-15 02:45:18 +0000

    Done.

--- index.js
+++ index.js
@@ -130,8 +130,12 @@ function moveDodger(e) {
    */
   if (e.which === LEFT_ARROW){
     moveDodgerLeft();
+    e.preventDefault()
+    e.stopPropagation()
   } else if (e.which === RIGHT_ARROW){
     moveDodgerRight();
+    e.preventDefault()
+    e.stopPropagation()
   }
 }
 
