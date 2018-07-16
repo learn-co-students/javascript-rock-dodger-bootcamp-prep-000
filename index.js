0e76e7a49f4a30f3b444621e4debd6db8459c631
Viktor Ivanov <sessgreem@gmail.com>
2018-07-16 19:59:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-/**
- * Don't change these constants!
- */
 const DODGER = document.getElementById('dodger')
 const GAME = document.getElementById('game')
 const GAME_HEIGHT = 400
 const GAME_WIDTH = 400
-const LEFT_ARROW = 37 // use e.which!
-const RIGHT_ARROW = 39 // use e.which!
+const LEFT_ARROW = 37 
+const RIGHT_ARROW = 39
 const ROCKS = []
 const START = document.getElementById('start')
 
 var gameInterval = null
 
-/**
- * Be aware of what's above this line,
- * but all of your work should happen below.
- */
-
 function checkCollision(rock) {
-  // implement me!
-  // use the comments below to guide you!
   const top = positionToInteger(rock.style.top)
 
   // rocks are 20px high
@@ -28,28 +18,11 @@ function checkCollision(rock) {
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
     const dodgerRightEdge = positionToInteger(DODGER.style.left) + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
     const rockRightEdge = positionToInteger(rock.style.left) + 20;
-
-//     if (false /**
-//                * Think about it -- what's happening here?
-//                * There's been a collision if one of three things is true:
-//                * 1. The rockleftedge < DODGERleftedge,
-//                *    and the rockrightedge > DODGERleftedge; rockLeftEdge < dodgerLeftEdge and rockRightEdge > dodgerLeftEdge 
-//                * 2. The rockleftedge > DODGERleftedge, 
-//                *    and the rockrightedge < DODGERrightedge; rockLeftEdge > dodgerLeftEdge and rockRightEdge < dodgerRightEdge
-//                * 3. The rockleftedge < DODGERrightedge,
-//                *    and the rockrightedge > DODGERrightedge  rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge
-//                */) {
-//       return true
-//     }
-//   }
-// }
 
   if((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) || (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge)
    ||(rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)){
@@ -57,7 +30,6 @@ function checkCollision(rock) {
   }
 }
 }
-//l/et index = 0;
 
 function createRock(x) {
   const rock = document.createElement('div')
@@ -86,33 +58,23 @@ function createRock(x) {
     }
   }
    window.requestAnimationFrame(moveRock)
-  // We should kick of the animation of the rock around here
-
-  // Add the rock to ROCKS so that we can remove all rocks
-  // when there's a collision
+ 
   ROCKS.push(rock)
 
-  // Finally, return the rock element you've created
   return rock
   
 }
-///let rocks = document.querySelectorAll('.rock')
-  // for(let i = 0 ;i<rocks.length;i++){
-  //   rock[i].remove();
-  // }
-/**
- * End the game by clearing `gameInterval`,
- * removing all ROCKS from the DOM,
- * and removing the `moveDodger` event listener.
- * Finally, alert "YOU LOSE!" to the player.
- */
+
 function endGame() {
   clearInterval(gameInterval)
   ROCKS.forEach(function(rock){
     rock.remove();
   })
+  
 document.removeEventListener('keydown',moveDodger)
-
+window.alert('You lose! :)')
+START.innerHTML = 'Play again?'
+START.style.display = 'inline'
 }
 
 function moveDodger(e) {
