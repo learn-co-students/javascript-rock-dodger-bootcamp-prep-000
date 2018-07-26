8277498c11eb6c9d7abedb9c378ef06e7b7766ae
bnreine <41244421+bnreine@users.noreply.github.com>
2018-07-26 14:43:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ var gameInterval = null
  * but all of your work should happen below.
  */
 
+
+
+
+
+
+/*
+
 function checkCollision(rock) {
   // implement me!
   // use the comments below to guide you!
@@ -46,34 +53,62 @@ function checkCollision(rock) {
                *    and the rock's right edge is < the DODGER's right edge;
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
-               */) {
+               */
+ /*              
+              ){
       return true
     }
   }
 }
 
+
+*/
+
+
+
+
+
+
+
+
 function createRock(x) {
   const rock = document.createElement('div')
-
+  GAME.append(rock)
+  
   rock.className = 'rock'
-  rock.style.left = `${x}px`
+  rock.style.left = `${x}px`  //left side of rock assigned to parameter into function
 
-  // Hmmm, why would we have used `var` here?
+  // Hmmm, why would we have used `var` here?  We need to reassign a new top for each new rock so they all start at the top.
   var top = 0
-
   rock.style.top = top
-
-  /**
+  moveRock()
+  
+  
+  /*
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
+
+}  
+  
+  
+  
+  
+
+  
+  
+
 
 
   /**
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
    */
-  function moveRock() {
+ 
+ 
+
+ 
+function moveRock() {
     // implement me!
     // (use the comments below to guide you!)
     /**
@@ -86,11 +121,25 @@ function createRock(x) {
      * the GAME, we want to move it again.
      */
 
+
+/*
+  function step() {
+    if (top <3) {
+      rock.style.top = `${left += 2}px`
+      window.requestAnimationFrame(step)
+    }
+  }
+
+  window.requestAnimationFrame(step)
+
+  */
+
+
     /**
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
-  }
+  
 
   // We should kick of the animation of the rock around here
 
@@ -99,8 +148,13 @@ function createRock(x) {
   ROCKS.push(rock)
 
   // Finally, return the rock element you've created
-  return rock
+//  return rock
 }
+
+
+
+
+
 
 /**
  * End the game by clearing `gameInterval`,
@@ -110,11 +164,14 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval)
-  for (var i=0;i<ROCKS.length;i++) {
+  for (var i=(ROCKS.length-1);i>=0;i--) {
     ROCKS[i].remove()
   }
-  
+  window.removeEventListener('keydown', moveDodger)
+  alert('YOU LOSE!')
 }
+
+
 
 function moveDodger(e) {
   // implement me!
@@ -205,19 +262,7 @@ function start() {
 
 
 /*
-function move(el) {
-  var top = 0
-
-  function step() {
-    el.style.top = `${top += 2}px`
-
-    if (top < 200) {
-      window.requestAnimationFrame(step)
-    }
-  }
-
-  window.requestAnimationFrame(step)
-}
+Random stuff at bottom, not to be used in actual js file
 
 
 
@@ -235,3 +280,4 @@ interval from executing by calling `clearInterval(myInterval)`.
 
 
 */
+
