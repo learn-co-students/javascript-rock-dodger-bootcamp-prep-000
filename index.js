d3a13bba8376ec24e133f1ff3c403a82b092e6ea
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-30 23:26:31 +0000

    Done.

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ if (left > 0){
 
 function moveDodgerRight() {
 window.requestAnimationFrame(function(){
-  var rightNumbers = DODGER.style.left.replace('','px')
+  var rightNumbers = DODGER.style.left.replace('px','')
   var left = parseInt(rightNumbers, 10)
 if (left < GAME_WIDTH-40){
   DODGER.style.left = `${left + 4}px`
