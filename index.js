0eb13e9ee8bebf4dff122c9d8925a3247f2da63b
Jae Park <wjp241@nyu.edu>
2018-09-12 02:29:15 +0000

    Done.

--- index.js
+++ index.js
@@ -138,6 +138,6 @@ function start() {
   window.addEventListener('keydown', moveDodger);
   START.style.display = 'none';
   gameInterval = setInterval(function() {
-    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  }, 1000)
+    createRock(Math.floor(Math.random() * (GAME_WIDTH - 20)))
+  },1000)
 }
