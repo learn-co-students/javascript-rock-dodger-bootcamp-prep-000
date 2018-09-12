8e8249a959455fbea926c8182c4889f85b1f900c
Jae Park <wjp241@nyu.edu>
2018-09-12 02:01:09 +0000

    Done.

--- index.js
+++ index.js
@@ -107,8 +107,12 @@ function endGame() {
  //And be sure to use the functions declared below!
 function moveDodger(e) {
   if (e.which === LEFT_ARROW) {
+    e.stopPropagation();
+    e.preventDefault();
     moveDodgerLeft();
   } else if (e.which === RIGHT_ARROW) {
+    e.stopPropagation();
+    e.preventDefault();
     moveDodgerRight();
   }
 }
