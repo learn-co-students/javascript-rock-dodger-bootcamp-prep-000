5a8446e8c9eb7b13226f49c1bbdba9276745e9b4
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-12 21:42:22 +0000

    Done.

--- index.js
+++ index.js
@@ -54,59 +54,33 @@ function checkCollision(rock) {
 
 function createRock(x) {
   const rock = document.createElement('div')
-
   rock.className = 'rock'
   rock.style.left = `${x}px`
-
   // Hmmm, why would we have used `var` here?
   var top = 0
-
   rock.style.top = top
-
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
+  //Now that we have a rock, we'll need to append it to GAME and move it downwards.
   GAME.appendChild(rock)
-
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
+  // This function moves the rock. (2 pixels at a time seems like a good pace.)
   function moveRock() {
-    // implement me!
-    // (use the comments below to guide you!)
-    while (!checkCollision(rock) && top < 360) { 
-      top += 2
+    // implement me! (use the comments below to guide you!)
+    function step() {
+      while (!checkCollision(rock) && top < 360) {
+        top += 2
+        window.requestAnimationFrame(step())
+      }
     }
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame()
-     */
-    if (checkCollision(rock)) {
-          endGame()
-    }
-        
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
-     if (top === 360) {
-       element.remove(rock)
-     }
+    window.requestAnimationFrame(step())
   }
-
-  // We should kick of the animation of the rock around here
-
+  if (checkCollision(rock)) {
+    endGame()
+  }
+  if (top >= 360) {
+    element.remove(rock)
+  }
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
-
   // Finally, return the rock element you've created
   return rock
 }
