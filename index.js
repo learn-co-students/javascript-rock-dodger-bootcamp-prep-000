59d9cd1b6e2621288efc329b3027fe46d62affdb
Jonathan Koss <jonathan_koss@yahoo.com>
2019-02-14 04:57:05 +0000

    Done.

--- index.js
+++ index.js
@@ -85,10 +85,10 @@ function createRock(x) {
      /* If a rock collides with the DODGER,
      * we should call endGame()
      */
-     rock.style.top = `${top2+=2}px`;
+     rock.style.top = `${top+=2}px`;
      if (checkCollision(rock)){
        return endGame();
-     } if (top2 < GAME_HEIGHT){
+     } if (top < GAME_HEIGHT){
        window.requestAnimationFrame(moveRock);
      } else {
        rock.remove();
