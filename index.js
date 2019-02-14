826f14977bc1852effff1332ecf84deef303b0b6
Jonathan Koss <jonathan_koss@yahoo.com>
2019-02-14 19:43:56 +0000

    Done.

--- index.js
+++ index.js
@@ -146,7 +146,8 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-}
+   document.addEventListener()
+    
 
 function moveDodgerLeft() {
   // implement me!
