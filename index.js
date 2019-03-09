98245759530bb62377bd3e17507e97136debdb7f
Jonathan Koss <jonathan_koss@yahoo.com>
2019-03-09 19:23:06 +0000

    Done.

--- index.js
+++ index.js
@@ -159,8 +159,12 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-
-  
+  window.requestAnimationFrame(function() {
+    let left = positionToInteger(DODGER.style.left);
+    if(left > 0) {
+      DODGER.style.left = `${left - 4}px`;
+    }
+  });
 }
 
 function moveDodgerRight() {
@@ -169,7 +173,12 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-   
+   window.requestAnimationFrame(function() {
+     let right = positionToInteger(DODGER.style.left);
+     if(right < 360) {
+      DODGER.style.left = `${right + 4}px`; 
+     }
+   });
 }
 
 /**
