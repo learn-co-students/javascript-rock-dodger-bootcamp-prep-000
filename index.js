223e721bdce99a67d590ba3becc92d85beef1311
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-05-02 19:06:31 +0000

    Done.

--- index.js
+++ index.js
@@ -50,31 +50,15 @@ function createRock(x) {
 
   rock.className = 'rock';
   rock.style.left = `${x}px`;
-
-  // Hmmm, why would we have used `var` here?
   var top = 0;
 
   rock.style.top = top;
   GAME.appendChild(rock);
   moveRock();
-  /*
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
-
-
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
+  
   function moveRock() {
     rock.style.top=`${top+=2}px`;
-    // implement me!
-    // (use the comments below to guide you!)
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame()
-     */
+    
      if(window.checkCollision(rock)){
        window.endGame();
      }
@@ -84,25 +68,9 @@ function createRock(x) {
      if(top==GAME_HEIGHT-24){
        rock.remove();
      }
-
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
   }
 
-  // We should kick of the animation of the rock around here
-
-  // Add the rock to ROCKS so that we can remove all rocks
-  // when there's a collision
   ROCKS.push(rock);
-
-  // Finally, return the rock element you've created
   return rock;
 }
 
@@ -114,9 +82,10 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval);
+  var len = ROCKS.length-3
   ROCKS.forEach(function(rock){ rock.remove()});
   document.removeEventListener('keydown',moveDodger);
-  START.innerHTML='Play again?';
+  START.innerHTML=`Play again? Score: ${len}`;
   START.style.display='inline';
 }
 
