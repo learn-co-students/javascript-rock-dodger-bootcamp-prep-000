4c4acc5ec778ac5a42acd428c59b8f0de75e955d
Mark Kauffman <markkauffman22@gmail.com>
2019-10-05 18:25:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
 
   
-/**
+/*
  * Don't change these constants!
  */
-const DODGER = document.getElementById('dodger')
-const GAME = document.getElementById('game')
-const GAME_HEIGHT = 400
-const GAME_WIDTH = 400
-const LEFT_ARROW = 37 // use e.which!
-const RIGHT_ARROW = 39 // use e.which!
-const ROCKS = []
-const START = document.getElementById('start')
+const DODGER = document.getElementById('dodger');
+const GAME = document.getElementById('game');
+const GAME_HEIGHT = 400;
+const GAME_WIDTH = 400;
+const LEFT_ARROW = 37; // use e.which!
+const RIGHT_ARROW = 39; // use e.which!
+const ROCKS = [];
+const START = document.getElementById('start');
 
-var gameInterval = null
+var gameInterval = null;
 
-/**
+/*
  * Be aware of what's above this line,
  * but all of your work should happen below.
  */
 
 function checkCollision(rock)
 {
-  const top = positionToInteger(rock.style.top)
+  const top = positionToInteger(rock.style.top);
 
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
   if (top > 360) {
-    const dodgerLeftEdge = positionToInteger(DODGER.style.left)
+    const dodgerLeftEdge = positionToInteger(DODGER.style.left);
 
     const dodgerRightEdge = dodgerLeftEdge + 40;
 
-    const rockLeftEdge = positionToInteger(rock.style.left)
+    const rockLeftEdge = positionToInteger(rock.style.left);
 
     const rockRightEdge = rockLeftEdge + 20;
 
-    if ((rockLeftEdge <= dodgerLeftEdge) && (rockRightEdge >= dodgerLeftEdge) || (rockLeftEdge <= dodgerRightEdge) && (rockRightEdge >= dodgerRightEdge) || (rockLeftEdge >= dodgerLeftEdge) && (rockRightEdge <= dodgerRightEdge))/**
-               * Think about it -- what's happening here?
+    if ((rockLeftEdge <= dodgerLeftEdge) && (rockRightEdge >= dodgerLeftEdge) || (rockLeftEdge <= dodgerRightEdge) && (rockRightEdge >= dodgerRightEdge) || (rockLeftEdge >= dodgerLeftEdge) && (rockRightEdge <= dodgerRightEdge))
+    /*
+               * What checkCollision needs to check for ...?
                * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
+                  * 1. The rock's left edge is < the DODGER's left edge,
+                  *    and the rock's right edge is > the DODGER's left edge;
+                  * 2. The rock's left edge is > the DODGER's left edge,
+                  *    and the rock's right edge is < the DODGER's right edge;
+                  * 3. The rock's left edge is < the DODGER's right edge,
+                  *    and the rock's right edge is > the DODGER's right edge
                */
     {
       return true;
@@ -53,10 +54,10 @@ function checkCollision(rock)
 
 function createRock(x)
 {
-  const rock = document.createElement('div')
-  rock.className = 'rock'
+  const rock = document.createElement('div');
+  rock.className = 'rock';
   rock.style.left = `${x}px`;
-  var top = 0
+  var top = 0;
   top = rock.style.top;
 
   /**
@@ -64,15 +65,14 @@ function createRock(x)
    * it to GAME and move it downwards.
    */
    //appends rock to game.
-   GAME.appendChild(rock)
+   GAME.appendChild(rock);
 
   /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
+   * This function moves the rock.
    */
 function moveRock()
   {
-     rock.style.top = `${top += 2}px`;
+     rock.style.top = `${top += 1}px`;
      //if a rock collides with the dodger, call end game function.
      if(checkCollision(rock)===true)
      {
@@ -90,14 +90,14 @@ function moveRock()
      }
   }
 
-  // We should kick of the animation of the rock around here
-window.requestAnimationFrame(moveRock)
+  // Start the animation of the rock around here ...
+window.requestAnimationFrame(moveRock);
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
-  ROCKS.push(rock)
+  ROCKS.push(rock);
 
   // Finally, return the rock element you've created
-  return rock
+  return rock;
 }
 
 /**
@@ -108,7 +108,7 @@ window.requestAnimationFrame(moveRock)
  */
 function endGame()
 {
-  clearInterval(gameInterval)
+  clearInterval(gameInterval);
   for(var i = 0; i < ROCKS.length; i++)
   {
     ROCKS[i].remove();
@@ -151,11 +151,11 @@ function moveDodgerLeft()
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
    window.requestAnimationFrame(function(){
-     const left = positionToInteger(DODGER.style.left)
+     const left = positionToInteger(DODGER.style.left);
      if(left > 0){
        DODGER.style.left = `${left - 4}px`;
      }
-   })
+   });
 }
 
 function moveDodgerRight() {
@@ -165,11 +165,11 @@ function moveDodgerRight() {
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
    window.requestAnimationFrame(function(){
-     const left = positionToInteger(DODGER.style.left)
+     const left = positionToInteger(DODGER.style.left);
      if(left < 360){
        DODGER.style.left = `${left + 4}px`;
      }
-   })
+   });
 }
 
 /**
@@ -177,17 +177,17 @@ function moveDodgerRight() {
  * @returns {number} The position as an integer (without 'px')
  */
 function positionToInteger(p) {
-  return parseInt(p.split('px')[0]) || 0
+  return parseInt(p.split('px')[0]) || 0;
 }
 
 function start()
 {
-  window.addEventListener('keydown', moveDodger)
+  window.addEventListener('keydown', moveDodger);
 
-  START.style.display = 'none'
+  START.style.display = 'none';
 
   gameInterval = setInterval(function() {
-    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  }, 2500)
+    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)));
+  }, 3000);
 }
 
