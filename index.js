0dbcfd2de2382f429a193263e23400dd996b2bbe
Eriberto Guzman <eriberto176@gmail.com>
2020-04-17 13:22:19 +0000

    Done.

--- index.js
+++ index.js
@@ -29,26 +29,28 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const rockRightEdge = rockLeftEdge + 20;
 
-    if (false /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge.
-               */) {
+    if(rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge)
+    {
       return true
     }
+    else if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge){
+      return true
+    }
+    else if (rockLeftEdge <= dodgerRightEdge && rockRightEdge>= dodgerRightEdge){
+      return true
+    }
+    else{
+      return false
+    }
   }
+  
 }
 
 function createRock(x) {
@@ -58,7 +60,7 @@ function createRock(x) {
   rock.style.left = `${x}px`
 
   // Hmmm, why would we have used `var` here?
-  var top = 0
+  var top = '0'
 
   rock.style.top = top
 
@@ -66,7 +68,7 @@ function createRock(x) {
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-
+  GAME.appendChild(rock)
 
   /**
    * This function moves the rock. (2 pixels at a time
@@ -89,15 +91,28 @@ function createRock(x) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM.
      */
+     
+     rock.style.top += `${top += 2}px`;
+     if (checkCollision(rock)){
+      endGame()
+       
+     }
+     else if(rock.style.top < 380){
+       window.requestAnimationFrame(moveRock)
+     }
+     else{
+       delete rock
+     }
   }
 
   // We should kick off the animation of the rock around here.
-
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision.
-  ROCKS.push(rock)
-
   // Finally, return the rock element you've created.
+  
+  window.requestAnimationFrame(moveRock)
+  ROCKS.push(rock)
+  
   return rock
 }
 
@@ -108,6 +123,13 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
+  clearInterval(gameInterval)
+  for(let i = 0; i < ROCKS.length; i++){
+    ROCKS[i].remove();
+  }
+  
+  alert("YOU LOSE!")
+  
 }
 
 function moveDodger(e) {
@@ -119,6 +141,19 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   
+   
+   if (e.which === LEFT_ARROW){
+     moveDodgerLeft();
+     e.preventDefault();
+     e.stopPropagation();
+   }
+   else if(e.which === RIGHT_ARROW){
+     moveDodgerRight();
+     e.preventDefault();
+     e.stopPropagation();
+   }
+   
 }
 
 function moveDodgerLeft() {
@@ -127,6 +162,18 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+  
+  var left = positionToInteger(DODGER.style.left)
+  
+  function step() {
+    
+    if(left > 0){
+      DODGER.style.left = `${left -= 4}px`
+      window.requestAnimationFrame(step)
+    }
+  }
+  window.requestAnimationFrame(step)
+  
 }
 
 function moveDodgerRight() {
@@ -135,6 +182,18 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+  var left = positionToInteger(DODGER.style.left)+40
+  
+  function step() {
+    
+    if(left < 400){
+      DODGER.style.left = `${left += 4}px`
+      window.requestAnimationFrame(step)
+    }
+  }
+  window.requestAnimationFrame(step)
+  
+
 }
 
 /**
